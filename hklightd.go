52da014780fa95589695eb76c8652e9115a99f9d
Jason Kusnier <jkusnier@users.noreply.github.com>
2016-05-08 21:05:02 -0400

    set env vars for PORT and IP

--- hklightd.go
+++ hklightd.go
@@ -33,7 +33,7 @@ func main() {
 		}
 	})
 
-	t, err := hc.NewIPTransport(hc.Config{Pin: "32191123", Port: "12345"}, acc.Accessory)
+	t, err := hc.NewIPTransport(hc.Config{Pin: "32191123", Port: os.Getenv("PORT"), IP: os.Getenv("IP")}, acc.Accessory)
 	if err != nil {
 		log.Fatal(err)
 	}
